3cbccb6d93433dbeb691998f23521879e0719f63
Nathan Knotts <nknotts@gmail.com>
2022-06-17 11:44:50 -0400

    skeleton klipper plugin

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from klipper.controller import NevermoreMaxController
+
+
+def load_config(config):
+    return NevermoreMaxController(config)
+
+
+def load_config_prefix(config):
+    return NevermoreMaxController(config)
---
+++ klipper/controller.py
@@ -0,0 +1,25 @@
+import logging
+
+from .serial import SerialPort
+from ..firmware import messagepacket
+
+
+class NevermoreMaxController:
+    def __init__(self, config):
+        serial_port = config.get("serial-port")
+        serial_baud = config.get("serial-baud", default=115200)
+        self.serial = SerialPort(serial_port, serial_baud)
+        self.serial_parser = messagepacket.MessageParser()
+
+        self.printer = config.get_printer()
+        self.printer.get_reactor().register_fd(self.serial.fd, self._serial_data_ready)
+
+    def _serial_data_ready(self, eventtime):
+        data = self.serial.read()
+        self.serial_parser.append(data)
+        msg = self.serial_parser.parse()
+        if msg:
+            self._received_message(msg)
+
+    def _received_message(self, msg: messagepacket.MessagePacket):
+        logging.info("Received NMC Msg", msg)
